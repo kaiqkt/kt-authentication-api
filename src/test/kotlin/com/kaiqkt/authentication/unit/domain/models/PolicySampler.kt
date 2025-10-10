40c6fc36a43540a68404a17c2a30d67856bddf6b
kaiqkt <kaiquebeserra1313@gmail.com>
2025-10-10 16:27:33 -0300

    - added creation policies

---
+++ src/test/kotlin/com/kaiqkt/authentication/unit/domain/models/PolicySampler.kt
@@ -0,0 +1,14 @@
+package com.kaiqkt.authentication.unit.domain.models
+
+import com.kaiqkt.authentication.domain.models.Policy
+import com.kaiqkt.authentication.domain.models.ResourceServer
+import com.kaiqkt.authentication.domain.models.enums.Method
+
+object PolicySampler {
+    fun sample(resourceServer: ResourceServer = ResourceServerSampler.sample()) = Policy(
+        uri = "/users",
+        method = Method.POST,
+        isPublic = false,
+        resourceServer = resourceServer
+    )
+}
